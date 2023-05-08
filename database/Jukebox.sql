c57de3caba7e8dbf41453ae3d659233708d2e895
srimadhi <srimadhi025@gmail.com>
2023-05-08 12:08:46 +0530

    database uploaded

--- database/Jukebox.sql
+++ database/Jukebox.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2023 at 10:39 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: May 08, 2023 at 08:37 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `spotify`
+-- Database: `jukebox`
 --
 
 -- --------------------------------------------------------
@@ -35,7 +35,7 @@ CREATE TABLE `music_list` (
   `image_path` text DEFAULT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `music_list`
@@ -43,7 +43,8 @@ CREATE TABLE `music_list` (
 
 INSERT INTO `music_list` (`id`, `title`, `description`, `audio_path`, `image_path`, `date_created`, `date_updated`) VALUES
 (2, 'Tere Bina', 'authors', './resources/audio/tere_bina.mp3?v=1683275047', './resources/images/0_music1.jpg?v=1683275047', '2023-05-05 13:30:49', '2023-05-05 13:54:07'),
-(3, 'Believer', 'authors', './resources/audio/believer.mp3?v=1683274806', './resources/images/0_music3.jpg?v=1683274806', '2023-05-05 13:50:06', '2023-05-05 13:50:06');
+(3, 'Believer', 'authors', './resources/audio/believer.mp3?v=1683274806', './resources/images/0_music3.jpg?v=1683274806', '2023-05-05 13:50:06', '2023-05-05 13:50:06'),
+(4, '3-movie', '3 BGM', './resources/audio/0_illayaraja.mp3?v=1683280262', './resources/images/1_0_music3.jpg?v=1683280263', '2023-05-05 15:21:02', '2023-05-05 15:21:03');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +64,7 @@ ALTER TABLE `music_list`
 -- AUTO_INCREMENT for table `music_list`
 --
 ALTER TABLE `music_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
